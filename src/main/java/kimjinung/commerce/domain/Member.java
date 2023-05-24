717f81cd7b6a4c069017a0de2c51bd64247a7693
KimJinung <KimJinung@outlook.com>
2023-05-24 13:25:21 +0900

    Add Member Entity

---
+++ src/main/java/kimjinung/commerce/domain/Member.java
@@ -0,0 +1,24 @@
+package kimjinung.commerce.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(
+            name = "uuid",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID uuid;
+
+    private String userId;
+    private String password;
+
+}
