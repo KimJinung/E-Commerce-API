00a363ee06c5e912678add23ec83afde401a66ca
KimJinung <KimJinung@outlook.com>
2023-05-25 15:53:39 +0900

    Add ShipmentRepositoryImpl

--- src/main/java/kimjinung/commerce/repository/shipment/ShipmentRepository.java
+++ src/main/java/kimjinung/commerce/repository/shipment/ShipmentRepository.java
@@ -1,13 +1,16 @@
 package kimjinung.commerce.repository.shipment;
 
+import kimjinung.commerce.domain.Order;
 import kimjinung.commerce.domain.Shipment;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ShipmentRepository {
 
     UUID save(Shipment shipment);
-    boolean remove(Shipment shipment);
     Optional<Shipment> findById(UUID uuid);
+
+    Optional<List<Shipment>> findByOrder(Order order);
 }
---
+++ src/main/java/kimjinung/commerce/repository/shipment/ShipmentRepositoryImpl.java
@@ -0,0 +1,41 @@
+package kimjinung.commerce.repository.shipment;
+
+import kimjinung.commerce.domain.Order;
+import kimjinung.commerce.domain.Shipment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Repository
+public class ShipmentRepositoryImpl implements ShipmentRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public UUID save(Shipment shipment) {
+        em.persist(shipment);
+        return shipment.getUuid();
+    }
+
+    @Override
+    public Optional<Shipment> findById(UUID uuid) {
+        Shipment shipment = em.find(Shipment.class, uuid);
+        return Optional.ofNullable(shipment);
+    }
+
+    @Override
+    public Optional<List<Shipment>> findByOrder(Order order) throws InvalidDataAccessApiUsageException {
+        String jpql = "SELECT s FROM Shipment s where s.order = :order";
+        List<Shipment> shipments = em.createQuery(jpql, Shipment.class)
+                .setParameter("order", order)
+                .getResultList();
+
+        return Optional.ofNullable(shipments);
+    }
+}
