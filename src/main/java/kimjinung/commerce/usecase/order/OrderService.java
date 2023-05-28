00b92fa0ed834fae624ee4327729fd7355cb1b82
KimJinung <KimJinung@outlook.com>
2023-05-28 16:11:09 +0900

    Add context comment

--- src/main/java/kimjinung/commerce/usecase/order/OrderService.java
+++ src/main/java/kimjinung/commerce/usecase/order/OrderService.java
@@ -11,4 +11,14 @@ public interface OrderService {
     OrderDTO order(OrderCreateDTO orderCreateDTO);
     List<OrderDTO> search(OrderSearchDTO orderSearchDTO);
     boolean cancel(OrderCancelDTO orderCancelDTO);
+
+    /*
+    각 서비스마다 적절한 DTO 클래스로 넘겨주어야한다.
+    결제 기능은 order 메서드 내에서 결제 API에 붙일 데이터를 가공하고, 호출한다.
+    API 콜 치고, 결과에 따라 결제 응답을 해야한다.
+
+    1. order
+    2. payment
+
+     */
 }
