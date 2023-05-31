eb88f4277e8b842bfebf626715edb68a97c451a5
KimJinung <KimJinung@outlook.com>
2023-05-31 21:05:37 +0900

    Add exception handling guideline

---
+++ src/main/java/kimjinung/commerce/exception/MemberJoinInvalidArgumentException.java
@@ -0,0 +1,7 @@
+package kimjinung.commerce.exception;
+
+public class MemberJoinInvalidArgumentException extends RuntimeException{
+    public MemberJoinInvalidArgumentException(String message) {
+        super(message);
+    }
+}
