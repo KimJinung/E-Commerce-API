9446c74ef8a6535dcd99bcd0af87e2b4096a2c8d
KimJinung <KimJinung@outlook.com>
2023-06-01 13:30:09 +0900

    Impl ProductServiceImpl

--- src/main/java/kimjinung/commerce/usecase/product/ProductService.java
+++ src/main/java/kimjinung/commerce/usecase/product/ProductService.java
@@ -3,11 +3,10 @@ package kimjinung.commerce.usecase.product;
 import kimjinung.commerce.dto.product.*;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface ProductService {
-    ProductDTO register(ProductRegisterDTO productRegisterDTO);
+    void register(ProductRegisterDTO productRegisterDTO);
     List<ProductDTO> search(ProductSearchDTO productSearchDTO);
-    ProductDTO update(ProductUpdateDTO productUpdateDTO);
-    boolean delete(ProductDeleteDTO productDeleteDTO);
+    void update(ProductUpdateDTO productUpdateDTO);
+    void delete(ProductDeleteDTO productDeleteDTO);
 }
