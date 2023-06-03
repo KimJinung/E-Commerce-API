75d8e6a7c7fd2be5405203537a718becf057f8dc
KimJinung <KimJinung@outlook.com>
2023-06-03 14:21:49 +0900

    Modify class name

--- src/main/java/kimjinung/commerce/exception/ItemNotExistException.java
+++
@@ -1,7 +0,0 @@
-package kimjinung.commerce.exception;
-
-public class ItemNotExistException extends RuntimeException {
-    public ItemNotExistException() {
-        super("Item not found");
-    }
-}
